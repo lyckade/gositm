3a823036954e3a4adf128fe8962203b43d8fd634
lyckade <as@lycka.de>
2016-06-13 11:16:31 +0200

    MakeFilename added

--- main.go
+++ main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"github.com/lyckade/gositm/conf"
+	"github.com/lyckade/gositm/timestamp"
+)
+
+var properties = conf.Properties
+
+var rootDir string
+
+func init() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	rootDir = dir
+}
+
+func main() {
+
+}
+
+// MakeFilename takes a filename and adds the timestamp before
+// the extension.
+// It is also possible to use a filepath as filename
+func MakeFilename(filename string, lastChange time.Time) string {
+	ext := path.Ext(filename)
+	ts := timestamp.FromTime(lastChange)
+	return fmt.Sprintf("%s.%s%s", filename[:len(filename)-len(ext)], ts, ext)
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/lyckade/gositm/timestamp"
+)
+
+func TestMakeFilename(t *testing.T) {
+	filename := "MyFile.txt"
+	lastChange := time.Now()
+	ts := timestamp.FromTime(lastChange)
+	expect := fmt.Sprintf("MyFile.%s.txt", ts)
+	got := MakeFilename(filename, lastChange)
+	if expect != got {
+		t.Fatalf("Expect: %v Got: %v", expect, got)
+	}
+}
